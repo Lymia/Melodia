873345b2d1bc523c0dfc555dff0c726f9d817438
Alissa Rao <lymia@lymiahugs.com>
2023-02-02 03:11:40 -0800

    Initial commit.

---
+++ CrystalLoad/CrystalLoad.cs
@@ -0,0 +1,24 @@
+﻿namespace CrystalLoad;
+
+using System;
+
+using SDL2;
+
+class CrystalLoad {
+    private static void ErrorMsgBox(string msg) {
+        SDL.SDL_ShowSimpleMessageBox(SDL.SDL_MessageBoxFlags.SDL_MESSAGEBOX_ERROR, "CrystalLoad Error", msg, IntPtr.Zero);
+        throw new Exception(msg);
+    }
+
+    static void Main(string[] args)
+    {
+        if (args.Length != 2) 
+            ErrorMsgBox("Not enough arguments passed to CrystalLoad Main!");
+
+        Console.WriteLine("CrystalLoad by AuroraAmissa");
+        Console.WriteLine();
+        Console.WriteLine($"Target .dll to load: {args[0]}");
+        Console.WriteLine($"Path to Crystal Project: {args[1]}");
+        Console.WriteLine();
+    }
+}
